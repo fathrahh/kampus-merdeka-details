fc89dae2bf8328a90537cafce5df040d9e4510a4
fathrahh <gazoon3@gmail.com>
2022-06-27 13:11:09 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import { BASE_URL_API_MAGANG } from "./utils/const";
+import { dateFormatter } from "./utils/dateFormatter";
 
 function App() {
+  const [dataMagang, setDataMagang] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const fetcher = async () => {
+    const {
+      data: { data },
+    } = await axios.get(BASE_URL_API_MAGANG);
+    setDataMagang(data);
+  };
+  const fetchRef = useRef(fetcher);
+
+  useEffect(() => {
+    fetchRef.current();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button onClick={fetcher}>Search</button>
+      <table>
+        <th>No</th>
+        <th>Name</th>
+        <th>Mitra</th>
+        <th>Activity Name</th>
+        <th>Kuota</th>
+        <th>Activity Type</th>
+        <th>Location</th>
+        <th>start regist</th>
+        <th>end regist</th>
+        {dataMagang.map((data, idx) => (
+          <tr>
+            <td>{idx + 1}</td>
+            <td>{data.name}</td>
+            <td>{data.mitra_name}</td>
+            <td>{data.activity_name}</td>
+            <td>{data.total}</td>
+            <td>{data.activity_type}</td>
+            <td>{data.location}</td>
+            <td>{dateFormatter(data.start_registration)}</td>
+            <td>{dateFormatter(data.end_registration)}</td>
+          </tr>
+        ))}
+      </table>
+      {/* <pre>{JSON.stringify(dataMagang, null, 2)}</pre> */}
     </div>
   );
 }
---
+++ src/utils/const.js
@@ -0,0 +1,2 @@
+export const BASE_URL_API_MAGANG =
+  "https://api.kampusmerdeka.kemdikbud.go.id/magang/browse/position?offset=0&limit=10000";
